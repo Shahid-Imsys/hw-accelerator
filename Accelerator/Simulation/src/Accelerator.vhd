52208c7e17b846a3d6caac54d6eff8f40b1142cc
boa <bengt.andersson@imsystech.com>
2020-09-10 16:58:36 +0200

    Update

---
+++ Accelerator/Simulation/src/Accelerator.vhd
@@ -0,0 +1,25 @@
+-- Top entity for Imsys AB Accelerator simulation
+-- 
+-- Design: Imsys AB
+-- Implemented: Bengt Andersson
+-- Revision 0
+
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_unsigned.all;
+
+library work;
+use work.defines.all;
+
+
+entity Accelerator is
+	port (
+	);
+end Accelerator;
+
+
+
+architecture struct of Accelerator is
+
+

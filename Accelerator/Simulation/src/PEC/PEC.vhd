a701a7f5148851a492b0530aa49520497786f6b0
boa <bengt.andersson@imsystech.com>
2020-09-10 20:27:18 +0200

    Added PEC folder

---
+++ Accelerator/Simulation/src/PEC/PEC.vhd
@@ -0,0 +1,27 @@
+-- PEC Part of NoC simulation pkg
+-- 
+-- Design: Imsys AB
+-- Implemented: Bengt Andersson
+-- Revision 0
+
+
+library ieee;
+use ieee.std_logic_1164.all;
+
+library work;
+use work.defines.all;
+
+
+
+entity PEC is 
+port (
+	
+);
+end entity PEC;
+
+
+
+architecture PEC_rtl of PEC is
+begin
+
+end architecture PEC_rtl;

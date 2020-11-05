ecb73b5a9db7a8d77110a98d91cf42dbe72ec6a5
azadeh kaffash <azadeh.kaffash@imsystech.com>
2020-11-05 10:18:28 +0100

    NoC_switch_commit

--- Accelerator/Simulation/src/NoC/NoC_switch.vhd
+++ Accelerator/Simulation/src/NoC/NoC_switch.vhd
@@ -1,25 +1,43 @@
--- NoC switch Part of NoC simulation pkg
--- 
--- Design: Imsys AB
--- Implemented: Bengt Andersson
--- Revision 0
---
+-------------------------------------------------------------------------------
+--                                                                           --
+--   COPYRIGHT (C)                       Imsys Technologies AB,  2005        --
+--                                                                           --
+--   The copyright to the document(s) herein is the property of Imsys        --
+--   Technologies AB, Sweden.                                                --
+--                                                                           --
+--   The document(s) may be used and/or copied only with the written         --
+--   permission from Imsys Technologies AB or in accordance with the         --
+--   terms and conditions stipulated in the agreement/contract under         --
+--   which the document(s) have been supplied.                               --
+--                                                                           --
+-------------------------------------------------------------------------------
+-- Title      : NoC_switch
+-- Project    : GP3000
+-------------------------------------------------------------------------------
+-- File       : NoC_switch.vhd
+-- Author     : Azadeh Kaffash
+-- Company    : Imsys Technologies AB
+-- Date       : 2020.11.04
+-------------------------------------------------------------------------------
+-- Description: -- NoC switch Part of NoC simulation pkg
 -- Switch settings:
--- 0000   no switch
--- 0001   broadcast
--- 0010   shift 1 byte left
--- 0011   shift 4 bytes left (16 x 4 regions)
--- 0100   shift 8 bytes left (8 x 8 regions)
--- 0101   interchange with byte +1
--- 0110   interchange with byte +2
--- 0111   interchange with byte +4
--- 1000   interchange with byte +8
--- 1001   interchange with byte +16
--- 1010   interchange with byte +32
-
-
-
-
+-- 000   no switch
+-- 001   broadcast
+-- 010   shift 1 byte left
+-- 011   shift 1 byte right
+-- 100   shift 4 byte left	
+-- 101   shift 8 bytes left
+-- 110   shift 4 byte right	
+-- 111   shift 8 byte right             
+-------------------------------------------------------------------------------
+-- TO-DO list : 
+--              
+--              
+-------------------------------------------------------------------------------
+-- Revisions  : 0
+-- Date					Version		Author	Description
+-- 2020-8-21  		     1.0	     CJ			Created
+-------------------------------------------------------------------------------
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.std_logic_unsigned.all;
@@ -32,8 +50,10 @@ entity switch is
 begin
 
 port (
-		enable	: in std_logic;
-
+		switch_In 		: in	NoC_bus;
+		decoder         : in	std_logic_vector(63 downto 0);
+		switch_mux		: in	std_logic_vector(3 downto 0);
+		switch_Out 		: out	NoC_bus
 	);
 end;
 
@@ -41,17 +61,62 @@ end;
 architecture struct of switch is
 begin
 
-	process low_mux()
-	
-	end low_mux;
+	switch_bus	: std_logic_vector(7 downto 0);
+	max_out     : NoC_bus;
 
+	process switch_bus_driver(decoder,switch_In)
+		begin
+		for i in 0 to 63 loop
+			if decoder(i) ='1' then
+				switch_bus <= switch_In(i);
+			end if;
+		end loop;
+	end process;
 
-	process mid_mux()
-	
-	end low_mux;
-
-
-	process high_mux()
-	
-	end low_mux;
-end struct switch;
+	process mux(switch_mux,switch_bus,switch_In)
+	begin
+		for i in 0 to 63 loop
+			if switch_mux = "000" then
+				mux_out[i] <= switch_In(i);
+			elsif switch_mux = "001" then
+				mux_out[i] <= switch_bus; ------------------------if it is changing at the same time it may be unstabel??
+			elsif switch_mux = "010" then
+				if (i > 0) then
+					mux_out[i] <= switch_In(i-1);
+				else
+					mux_out[i] <=(others => '0');
+				end;
+			elsif switch_mux = "011" then
+				if (i < 63) then
+					mux_out[i] <= switch_In(i+1);
+				else
+					mux_out[i] <=(others => '0');
+				end;
+			elsif switch_mux = "100" then
+				if (i > 4) then
+					mux_out[i] <= switch_In(i-4);
+				else
+					mux_out[i] <=(others => '0');
+				end;
+			elsif switch_mux = "101" then
+				if (i > 8) then
+					mux_out[i] <= switch_In(i-8);
+				else
+					mux_out[i] <=(others => '0');
+				end;
+			elsif switch_mux = "110" then
+				if (i < 60) then
+					mux_out[i] <= switch_In(i+4);
+				else
+					mux_out[i] <=(others => '0');
+				end;
+			elsif switch_mux = "111" then
+				if (i < 56) then
+					mux_out[i] <= switch_In(i+8);
+				else
+					mux_out[i] <=(others => '0');
+				end;		
+			end if;	
+		end;
+	end;
+end;

aee34b7332498457ccb31351a896ed43ebd3487a
Yongjun Liu <yongjun.liu@imsystech.com>
2022-03-02 14:06:58 +0100

    Added core2 memory test, fixed some bugs in microcode.

--- Processors/IM3000E/tb/tb_components/digital_sim_pack.vhd
+++ Processors/IM3000E/tb/tb_components/digital_sim_pack.vhd
@@ -141,29 +141,30 @@ package body digital_top_sim_pack is
 
 
       --Memory test error label:
-      when 16#B0# => write_code(core, "SB_ERR "); --G memory SB part read/write error 
-      when 16#B1# => write_code(core, "GR_ERR");  --G memory GR part read/write error
+      when 16#B0# => write_code(core, "GR_ERR");  --G memory GR part read/write error
+      when 16#B1# => write_code(core, "SB_ERR "); --G memory SB part read/write error 
       when 16#B2# => write_code(core, "MS_ERR");  --G memory MSTACK part read/write error
-      when 16#B5# => write_code(core, "PS_ERR");  --G memory PSTACK part read/write error 
-      when 16#B6# => write_code(core, "BMEM_ERR");  --Bettery memory read/write error 
-      when 16#B7# => write_code(core, "RAM0_ERR");  --RAM0 read/write error 
-      when 16#BB# => write_code(core, "RAM_ERR");   --RAM1-4 read/write error 
-      when 16#C2# => write_code(core, "IOMEM_ERR");  --IO memory read/write error 
-      
+      when 16#B3# => write_code(core, "PS_ERR");  --G memory PSTACK part read/write error 
+      when 16#B4# => write_code(core, "GR_ERR_CB "); 
+      when 16#B5# => write_code(core, "SB_ERR_CB");  
+      when 16#B6# => write_code(core, "MS_ERR_CB");  
+      when 16#B7# => write_code(core, "PS_ERR_CB");  
       when 16#B8# => write_code(core, "GR_ERR_INC "); 
       when 16#B9# => write_code(core, "SB_ERR_INC");  
       when 16#BA# => write_code(core, "MS_ERR_INC");  
-      when 16#BC# => write_code(core, "PS_ERR_INC");  
-      when 16#BD# => write_code(core, "BMEM_ERR_INC");  
+      when 16#BB# => write_code(core, "PS_ERR_INC"); 
 
-      when 16#BE# => write_code(core, "GR_ERR_CB "); 
-      when 16#BF# => write_code(core, "SB_ERR_CB");  
-      when 16#C7# => write_code(core, "MS_ERR_CB");  
-      when 16#C8# => write_code(core, "PS_ERR_CB");  
-      when 16#C9# => write_code(core, "BMEM_ERR_CB");  
+      when 16#BC# => write_code(core, "BMEM_ERR");  --Bettery memory read/write error 
+      when 16#BD# => write_code(core, "BMEM_ERR_CB");  
+      when 16#BE# => write_code(core, "BMEM_ERR_INC"); 
+
+      when 16#BF# => write_code(core, "RAM0_ERR");  --RAM0 read/write error 
+      when 16#C0# => write_code(core, "RAM_ERR");   --RAM1-4 read/write error 
+      when 16#C1# => write_code(core, "IOMEM_ERR");  --IO memory read/write error 
+    
 
       --Memory test status code
-      when 16#C1# => write_code(core, "GMEM: all test task end");  
+      when 16#C2# => write_code(core, "GMEM: all test task end");  
       when 16#C3# => write_code(core, "BMEM: all test task end");   
       when 16#C4# => write_code(core, "RAM0: all test task end");  
       when 16#C5# => write_code(core, "RAM: all test task end");  

f56194db57fd78b606abc4aa5d436cfafb393f0c
Bengt Svantesson <bengt.svantesson@imsystech.com>
2022-03-29 16:46:00 +0200

    Removed PLL

--- Processors/IM3000E/src/top.vhd
+++ Processors/IM3000E/src/top.vhd
@@ -157,31 +157,7 @@ entity top is
  end top;
 
 architecture struct of top is
- --PLL
-  component FXPLL031HA0A_APGD
-    port (
-      CKOUT   : out   std_logic;  -- PLL output to clock buffer
-      TCKO    : out   std_logic;
-      CIN     : in    std_logic;  -- Feedback from clock buffer output
-      FREF    : in    std_logic;  -- Reference clock input
-      TEST    : in    std_logic;
-      FRANGE  : in    std_logic;
-      PDN     : in    std_logic;  -- Power down PLL
-      TCKI    : in    std_logic;
-      NS0     : in    std_logic;  -- Frequency multiplier
-      NS1     : in    std_logic;
-      NS2     : in    std_logic;
-      NS3     : in    std_logic;
-      NS4     : in    std_logic;
-      NS5     : in    std_logic;
-      MS0     : in    std_logic;  -- Frequency divider
-      MS1     : in    std_logic;
-      MS2     : in    std_logic;
-      MS3     : in    std_logic;
-      MS4     : in    std_logic;
-      MS5     : in    std_logic);
-    end component;
-
+ 
   -- trcmem
   component SY180_256X32X1CM4
   port(
@@ -1321,28 +1297,32 @@ begin
   -----------------------------------------------------------------------------
   -- PLL
   -----------------------------------------------------------------------------
-  pll0: FXPLL031HA0A_APGD
-    port map (
-      FREF		    => hclk_i,
-      FRANGE	    => pll_frange,
-      NS0			    => pll_n(0),
-      NS1			    => pll_n(1),
-      NS2			    => pll_n(2),
-      NS3			    => pll_n(3),
-      NS4			    => pll_n(4),
-      NS5			    => pll_n(5),
-      MS0			    => pll_m(0),
-      MS1			    => pll_m(1),
-      MS2			    => pll_m(2),
-      MS3			    => const_0,
-      MS4			    => const_0,
-      MS5			    => const_0,
-      PDN			    => pll_pdn,
-      TCKO		    => tcko,
-      TEST		    => test_pll,
-      TCKI		    => hclk_i,
-      CKOUT		    => pllout,
-      CIN			    => pllout);
+  -- pll0: FXPLL031HA0A_APGD
+  --   port map (
+  --     FREF		    => hclk_i,
+  --     FRANGE	    => pll_frange,
+  --     NS0			    => pll_n(0),
+  --     NS1			    => pll_n(1),
+  --     NS2			    => pll_n(2),
+  --     NS3			    => pll_n(3),
+  --     NS4			    => pll_n(4),
+  --     NS5			    => pll_n(5),
+  --     MS0			    => pll_m(0),
+  --     MS1			    => pll_m(1),
+  --     MS2			    => pll_m(2),
+  --     MS3			    => const_0,
+  --     MS4			    => const_0,
+  --     MS5			    => const_0,
+  --     PDN			    => pll_pdn,
+  --     TCKO		    => tcko,
+  --     TEST		    => test_pll,
+  --     TCKI		    => hclk_i,
+  --     CKOUT		    => pllout,
+  --     CIN			    => pllout);
+
+    
+    pllout <= hclk_i;
+    tcko <= '0';
   pll_pdn <= en_pll or (not xout_selected);--pll can only be reset when pll is not selected (xout is selected)
 	const_0 <= '0';
  

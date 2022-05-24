ca3100d94599a152af5a0415b760bbe411cd7a53
Jens Mellberg <jens.mellberg@shortlink.se>
2022-05-24 09:17:11 +0200

    Added rom_bin_to_hex_conv that convert the mprom files for IM4000 from ascii binary file to a hex file.

---
+++ IP/Synopsys/rom_bin_to_hex_conv/readmemh.v
@@ -0,0 +1,14 @@
+`timescale 10ns/1ns
+module rom_bin_to_hex_conv;
+   
+integer i;
+reg [79:0] memory [0:4095]; // 80 bit memory with 4096 entries
+
+initial begin
+   $readmemb("../Embed-it_Integrator/mprom0.data",memory);
+   $writememh("../Embed-it_Integrator/mprom0.hex", memory);
+   $readmemb("../Embed-it_Integrator/mprom1.data",memory);
+   $writememh("../Embed-it_Integrator/mprom1.hex", memory);
+end
+endmodule;
+   

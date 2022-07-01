4274433fff801106c22acc32ef439e4ced713cf8
Markus Karlsson <markus.karlsson@imsystech.com>
2022-07-01 08:49:44 +0200

    Fixed bad component

--- Accelerator/Emulator_version_demo/src/NoC2/Root_Memory.vhd
+++ Accelerator/Emulator_version_demo/src/NoC2/Root_Memory.vhd
@@ -1,21 +1,21 @@
 ----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
+-- Company:
+-- Engineer:
+--
 -- Create Date: 04.04.2022 17:39:04
--- Design Name: 
+-- Design Name:
 -- Module Name: Root_Memory - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
+-- Project Name:
+-- Target Devices:
+-- Tool Versions:
+-- Description:
+--
+-- Dependencies:
+--
 -- Revision:
 -- Revision 0.01 - File Created
 -- Additional Comments:
--- 
+--
 ----------------------------------------------------------------------------------
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
@@ -33,7 +33,7 @@ entity Root_Memory is
         Load_RM_Address     : in  std_logic;
         RM_Address          : in  std_logic_vector(12 downto 0);
         DataIn              : in  std_logic_vector(127 downto 0);
-        DataOut             : out std_logic_vector(127 downto 0)  
+        DataOut             : out std_logic_vector(127 downto 0)
    );
 end Root_Memory;
 
@@ -44,62 +44,39 @@ architecture Behavioral of Root_Memory is
     signal RM_FF        : std_logic;
     signal Enable_p     : std_logic;
     signal Enable_p2    : std_logic;
-    signal mem_enable   : std_logic; 
+    signal mem_enable   : std_logic;
 
-  component CMEM_32KX16 is
-    port(
-      addr_c : in std_logic_vector(14 downto 0);
-      CK     : in std_logic;
-      WR     : in std_logic;
-      RD     : in std_logic;
-      DI0    : in std_logic_vector(7 downto 0);
-      DI1    : in std_logic_vector(7 downto 0);
-      DI2    : in std_logic_vector(7 downto 0);
-      DI3    : in std_logic_vector(7 downto 0);
-      DI4    : in std_logic_vector(7 downto 0);
-      DI5    : in std_logic_vector(7 downto 0);
-      DI6    : in std_logic_vector(7 downto 0);
-      DI7    : in std_logic_vector(7 downto 0);
-      DI8    : in std_logic_vector(7 downto 0);
-      DI9    : in std_logic_vector(7 downto 0);
-      DI10   : in std_logic_vector(7 downto 0);
-      DI11   : in std_logic_vector(7 downto 0);
-      DI12   : in std_logic_vector(7 downto 0);
-      DI13   : in std_logic_vector(7 downto 0);
-      DI14   : in std_logic_vector(7 downto 0);
-      DI15   : in std_logic_vector(7 downto 0);
-
-      DO0  : out std_logic_vector(7 downto 0);
-      DO1  : out std_logic_vector(7 downto 0);
-      DO2  : out std_logic_vector(7 downto 0);
-      DO3  : out std_logic_vector(7 downto 0);
-      DO4  : out std_logic_vector(7 downto 0);
-      DO5  : out std_logic_vector(7 downto 0);
-      DO6  : out std_logic_vector(7 downto 0);
-      DO7  : out std_logic_vector(7 downto 0);
-      DO8  : out std_logic_vector(7 downto 0);
-      DO9  : out std_logic_vector(7 downto 0);
-      DO10 : out std_logic_vector(7 downto 0);
-      DO11 : out std_logic_vector(7 downto 0);
-      DO12 : out std_logic_vector(7 downto 0);
-      DO13 : out std_logic_vector(7 downto 0);
-      DO14 : out std_logic_vector(7 downto 0);
-      DO15 : out std_logic_vector(7 downto 0)
-
-      );
+  component SNPS_SP_HD_8Kx128 is
+    port (
+      Q        : out std_logic_vector(127 downto 0);
+      ADR      : in  std_logic_vector(12 downto 0);
+      D        : in  std_logic_vector(127 downto 0);
+      WE       : in  std_logic;
+      ME       : in  std_logic;
+      CLK      : in  std_logic;
+      TEST1    : in  std_logic;
+      TEST_RNM : in  std_logic;
+      RME      : in  std_logic;
+      RM       : in  std_logic_vector(3 downto 0);
+      WA       : in  std_logic_vector(1 downto 0);
+      WPULSE   : in  std_logic_vector(2 downto 0);
+      LS       : in  std_logic;
+      BC0      : in  std_logic;
+      BC1      : in  std_logic;
+      BC2      : in  std_logic);
   end component;
 
-    component blk_mem_gen_0
-      port (
-        clka    : in std_logic;
-        ena     : in std_logic;
-        wea     : in std_logic_vector(0 downto 0);
-        addra   : in std_logic_vector(12 downto 0);
-        dina    : in std_logic_vector(127 downto 0);
-        douta   : out std_logic_vector(127 downto 0)
-      );
-    end component;
-  
+  component blk_mem_gen_0
+    port (
+      clka    : in std_logic;
+      ena     : in std_logic;
+      wea     : in std_logic_vector(0 downto 0);
+      addra   : in std_logic_vector(12 downto 0);
+      dina    : in std_logic_vector(127 downto 0);
+      douta   : out std_logic_vector(127 downto 0)
+    );
+  end component;
+
 begin
 
     we(0)       <= Write_Read_Mode;
@@ -119,11 +96,11 @@ begin
             Enable_p2     <= Enable_p;
         end if;
     end process;
-    
+
     mem_enable <= (Enable or Enable_p or Enable_p2) when Write_Read_Mode = '0' else Enable;
-    
-    if USE_ASIC_MEMORIES generate
-    
+
+rm_asic_gen : if USE_ASIC_MEMORIES generate
+
     Root_Memory_Inst : SNPS_SP_HD_8Kx128
         port map (
           Q        => DataOut,
@@ -142,10 +119,9 @@ begin
           BC0      => '0',
           BC1      => '0',
           BC2      => '0');
-
     end generate;
-    
-    if not USE_ASIC_MEMORIES generate
+
+rm_sim_gen : if not USE_ASIC_MEMORIES generate
 
     Root_Memory_Inst : blk_mem_gen_0
     port map (
@@ -156,7 +132,6 @@ begin
         dina    => DataIn,
         douta   => DataOut
     );
-    
     end generate;
 
 end Behavioral;

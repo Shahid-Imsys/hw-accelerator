ca24a2e2e1b4a518b4dbcb2473d7dc5b6d3df109
Markus Karlsson <markus.karlsson@imsystech.com>
2022-07-07 08:35:03 +0200

    Entity cleanup

--- Digital_core/src/ionoc.vhd
+++ Digital_core/src/ionoc.vhd
@@ -60,7 +60,7 @@ entity ionoc is
     ------------------------------------------------------
 
 
-    -- Domain clk_noc
+    -- Domain clk_noc (NOC)
     ------------------------------------------------------
     clk_noc       : in  std_logic;                      -- NOC Clock
     -- GPP CMD to NOC
@@ -71,6 +71,15 @@ entity ionoc is
     NOC_CMD_Flag  : in  std_logic;                      -- NOC command byte is valid
     NOC_CMD       : in  std_logic_vector(7 downto 0);   -- Command byte
     GPP_CMD_ACK   : out std_logic;                      -- GPP ack of command byte
+    -- NOC Data interface - for NOC request of data trx
+    NOC_ADDRESS   : in  std_logic_vector(31 downto 0);  -- Memory address of NOC data request
+    NOC_LENGTH    : in  std_logic_vector(15 downto 0);  -- Length of NOC data request
+    NOC_DATA_DIR  : in  std_logic;                      -- Direction of NOC data request
+    NOC_WRITE_REQ : in  std_logic;                      -- NOC address, length and data direction is valid
+    IO_WRITE_ACK  : out std_logic;                      -- NOC data parameters have been read and can now be updated
+
+    -- Domain clk_noc (FIFOS)
+    ------------------------------------------------------
     -- NOC TxFIFO, read by IONOC
     TxFIFO_Ready  : out std_logic;                      -- Interface can accept a word from the TxIFO
     TxFIFO_Valid  : in  std_logic;                      -- TxFIFO has availble data which is presented on bus
@@ -78,13 +87,7 @@ entity ionoc is
     -- NOC RxFIFO, written to by IONOC
     RxFIFO_Ready  : in  std_logic;                      -- RxFIFO can accept a word from the IO-bus
     RxFIFO_Valid  : out std_logic;                      -- Interface has availble data which is presented on bus
-    RxFIFO_Data   : out std_logic_vector(127 downto 0); -- RxFIFO data
-    -- NOC Data interface - for NOC request of data trx
-    NOC_ADDRESS   : in  std_logic_vector(31 downto 0);  -- Memory address of NOC data request
-    NOC_LENGTH    : in  std_logic_vector(15 downto 0);  -- Length of NOC data request
-    NOC_DATA_DIR  : in  std_logic;                      -- Direction of NOC data request
-    NOC_WRITE_REQ : in  std_logic;                      -- NOC address, length and data direction is valid
-    IO_WRITE_ACK  : out std_logic                       -- NOC data parameters have been read and can now be updated
+    RxFIFO_Data   : out std_logic_vector(127 downto 0)  -- RxFIFO data
     ------------------------------------------------------
     );
 end ionoc;

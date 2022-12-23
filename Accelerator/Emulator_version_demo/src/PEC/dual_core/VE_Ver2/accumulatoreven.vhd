ffe0cfd53a6f93cc71da2124d7fc3671ecd4c6b6
Zejun <zejun.huang@imsystech.com>
2022-12-23 15:36:04 +0100

    maxpool fix.

--- Accelerator/Emulator_version_demo/src/PEC/dual_core/VE_Ver2/accumulatoreven.vhd
+++ Accelerator/Emulator_version_demo/src/PEC/dual_core/VE_Ver2/accumulatoreven.vhd
@@ -41,7 +41,7 @@ begin
   mul_odd_abs <= signed(shift_left(resize(unsigned(not(mul_odd(14 downto 7))), 31), 7)) & '1' when sign = '1' else
                  signed(shift_left(resize(unsigned(mul_odd(14 downto 7)), 31), 7)) & '0';
 
-  add_input0 <= mul_abs when ctrl.add = abs16 else resize(mul(17 downto 1) & '1', 32) when ctrl.add = max else resize(mul, 32);
+  add_input0 <= mul_abs when ctrl.add = abs16 else resize(mul, 32);
 
   with ctrl.add select add_input1 <=
     czero                              when zero,

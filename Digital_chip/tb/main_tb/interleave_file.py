cebcacd94f901181c1d8d09a695ada1c032deb1a
Markus Karlsson <markus.karlsson@imsystech.com>
2022-12-29 12:27:37 +0100

    Fix of bad fix =)

--- Digital_chip/tb/main_tb/interleave_file.py
+++ Digital_chip/tb/main_tb/interleave_file.py
@@ -7,34 +7,39 @@ import sys
 import os
 
 def write_file_bytes(filename, data):
-    with open(filename, "wb") as file:
-        file.write( bytearray( data ) )
+    with open(filename, "wt") as file:
+        for s in data:
+            file.write( s )
         print("Wrote " + str( len( data ) ) + " bytes to " + filename )
 
-byteArray = []
+bitArray = []
+rowArray = []
 
 prefix = 'interleaved_part'
 suffix = '.mif'
+byte   = ''
 
 if len( sys.argv ) > 1:
     prefix = sys.argv[1]
 
-add_newline = False
 for line in sys.stdin:
     for ch in line:
         n = ord( ch )
-        if n == 13:
-            pass
-        elif n == 10:
-            if add_newline:
-                byteArray.append( n )
-                byteArray.append( n )
-            add_newline = not add_newline
-        else:
-            byteArray.append( n )
+        if n != 10 and n != 13:
+            bitArray.append( ch )
+
+for b, bit in enumerate( bitArray ):
+    if (b % 8) == 0:
+        byte = ''
+    byte = byte + bit
+
+    if (b % 8) == 7:
+        byte = byte + chr( 10 )
+        rowArray.append( byte )
+        # cByte = (cByte + 1) % 8
 
 file0 = prefix + '0' + suffix
 file1 = prefix + '1' + suffix
 
-data0 = write_file_bytes( file0, byteArray[0::2] )
-data1 = write_file_bytes( file1, byteArray[1::2] )
+data0 = write_file_bytes( file0, rowArray[0::2] )
+data1 = write_file_bytes( file1, rowArray[1::2] )

927de4bca82e543a5eb133c291f76716c06437ad
Zejun <zejun.huang@imsystech.com>
2023-01-02 16:15:45 +0100

    "core ready" reset fix.

--- Accelerator/Emulator_version_demo/src/PEC/dual_core/core2/pe1_acore.vhd
+++ Accelerator/Emulator_version_demo/src/PEC/dual_core/core2/pe1_acore.vhd
@@ -383,7 +383,7 @@ begin
     if rising_edge(clk_p) then--rising_edge(clk_e)
       core2_en_buf <= core2_en;
       if rst_en_int = '0' then
-        pl <= x"8" & x"0000000000000000000000000000000";
+        pl <= x"00000000000000000000000000000000";
         core2_en_buf <= '0';
       elsif clk_e_pos_int = '0' then
         pl <= mp_q;

4b20a58ef73130789bf840d2849b8f013bd83c20
Zejun Huang <zejun.huang@imsystech.com>
2023-01-18 10:44:13 +0100

    Makefile update

--- Accelerator/Emulator_version_demo/src/PEC/testbench/AccBypass_short_tb.vhd
+++ Accelerator/Emulator_version_demo/src/PEC/testbench/AccBypass_short_tb.vhd
@@ -232,7 +232,7 @@ architecture Behavioral of Accelerator_tb is
       return RAM;
     end function;
 
-    signal program_mem_data  : program_mem_type := init_program_mem_from_file("tb_program_mem_code_regen_0.ascii");
+    signal program_mem_data  : program_mem_type := init_program_mem_from_file("tb_program_mem_code_regen_2.ascii");
     signal data_Input        : data_in_type := init_input_from_file("tb_input_data.ascii");
     signal Root_mem_data     : Root_mem_data_type := init_Root_mem_from_file("tb_Root_mem_data.ascii");       	   
     
